92b95a421d894f3f46b27ee4b18aa384f3a92400
Haydini <hayden.goff13@gmail.com>
2026-09-18 20:33:13 -0500

    Resource.h and Resource.cpp created

--- include/Resource.h
+++ include/Resource.h
@@ -1,1 +1,28 @@
-#pragma once
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+#include <string>
+using namespace std;
+
+class Resource {
+private:
+	string resourceID;
+	string resourceName;
+	string resourceType;
+	string availabilityStatus;
+
+public:
+	Resource();
+
+	string getResourceID();
+	string getResourceName();
+	string getResourceType();
+	string getAvailabilityStatus();
+
+	void setResourceID(string id);
+	void setResourceName(string name);
+	void setResourceType(string type);
+	void setAvailabilityStatus(string status);
+};
+
+#endif // RESOURCE_H
