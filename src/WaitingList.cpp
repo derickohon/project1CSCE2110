1956ab99c9883b329bedff35ab453e3d373130ef
ac2526 <AbhijeethChouti@my.unt.edu>
2026-09-20 13:49:58 -0500

    Refactor WaitingList class and its methods

--- src/WaitingList.cpp
+++ src/WaitingList.cpp
@@ -3,52 +3,51 @@
 
 using namespace std;
 
-// WaitingRequest default constructor
+
 WaitingRequest::WaitingRequest() {
     studentID = 0;
     studentName = "";
     resourceID = "";
 }
 
-// WaitingRequest parameterized constructor
+
 WaitingRequest::WaitingRequest(int id, string name, string resource) {
     studentID = id;
     studentName = name;
     resourceID = resource;
 }
 
-// WaitingNode constructor
+
 WaitingNode::WaitingNode(WaitingRequest req) {
     request = req;
     next = nullptr;
 }
 
-// WaitingList constructor
+
 WaitingList::WaitingList() {
     front = nullptr;
     rear = nullptr;
     count = 0;
 }
 
-// WaitingList destructor
+
 WaitingList::~WaitingList() {
     clear();
 }
 
-// Adds a student to the back of the waiting queue.
+
 void WaitingList::addStudent(int studentID, string studentName, string resourceID) {
     WaitingRequest request(studentID, studentName, resourceID);
 
     WaitingNode* newNode = new WaitingNode(request);
 
-    // If the queue is empty, the new node becomes
-    // both the front and rear.
+    
     if (isEmpty()) {
         front = newNode;
         rear = newNode;
     }
     else {
-        // Add the new node after the current rear.
+    
         rear->next = newNode;
         rear = newNode;
     }
@@ -56,7 +55,7 @@ void WaitingList::addStudent(int studentID, string studentName, string resourceI
     count++;
 }
 
-// Removes the student at the front of the waiting queue.
+
 bool WaitingList::removeStudent(WaitingRequest& request) {
     // Cannot remove a student from an empty queue.
     if (isEmpty()) {
@@ -64,36 +63,29 @@ bool WaitingList::removeStudent(WaitingRequest& request) {
     }
 
     WaitingNode* temp = front;
-
-    // Copy the request before deleting the node.
+ 
     request = temp->request;
 
-    // Move front to the next student.
     front = front->next;
 
     delete temp;
     count--;
 
-    // If the last student was removed,
-    // reset rear as well.
-    if (front == nullptr) {
+     if (front == nullptr) {
         rear = nullptr;
     }
 
     return true;
 }
 
-// Checks whether the waiting queue is empty.
 bool WaitingList::isEmpty() const {
     return front == nullptr;
 }
 
-// Returns the number of students currently waiting.
 int WaitingList::size() const {
     return count;
 }
 
-// Displays all students in FIFO order.
 void WaitingList::display() const {
     if (isEmpty()) {
         cout << "Waiting list is empty." << endl;
@@ -119,7 +111,6 @@ void WaitingList::display() const {
     cout << "Total waiting: " << count << endl;
 }
 
-// Removes every node from the waiting queue.
 void WaitingList::clear() {
     while (front != nullptr) {
         WaitingNode* temp = front;
